f18cd84a2dd4e000fb00df44593f9239ecf776db
HikaruTakakura <takakurahikaru@gmail.com>
2020-11-10 23:39:08 +0900

    sepalate EditForms

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <UserAuth />
-    <UserProfileForm v-if="$store.state.user" />
+    <div v-if="$store.state.user">
+      <UserNameEditForm />
+      <UserImageEditForm />
+    </div>
   </div>
 </template>
 
 <script>
 import UserAuth from "@/components/UserAuth.vue"
-import UserProfileForm from "@/components/UserProfileForm.vue"
+import UserNameEditForm from "@/components/UserNameEditForm.vue"
+import UserImageEditForm from "@/components/UserImageEditForm.vue"
 
 export default {
   components: {
     UserAuth,
-    UserProfileForm,
+    UserNameEditForm,
+    UserImageEditForm,
   },
 }
 </script>
